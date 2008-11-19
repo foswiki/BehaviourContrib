eac0d48f4b2922a32d58bcfbbdc7e73465be1cad
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-11-19 15:00:00 +0000

    Item175: port a subset of the standard extensions over to the new namespace to make a working system with mixed TWiki and Foswiki plugins
    git-svn-id: http://svn.foswiki.org/trunk/BehaviourContrib@811 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/BehaviourContrib.pm
+++ lib/Foswiki/Contrib/BehaviourContrib.pm
@@ -1,11 +1,11 @@
-package TWiki::Contrib::BehaviourContrib;
+package Foswiki::Contrib::BehaviourContrib;
 use vars qw( $VERSION );
 $VERSION = '$Rev$';
 $RELEASE = '1.3.1';
 
 =begin twiki
 
----+++ TWiki::Contrib::BehaviourContrib::addHEAD()
+---+++ Foswiki::Contrib::BehaviourContrib::addHEAD()
 
 This function will automatically add the headers for the contrib to
 the page being rendered. It is intended for use from Plugins and
@@ -14,8 +14,8 @@ other extensions. For example:
 <verbatim>
 sub commonTagsHandler {
   ....
-  require TWiki::Contrib::BehaviourContrib;
-  TWiki::Contrib::BehaviourContrib::addHEAD();
+  require Foswiki::Contrib::BehaviourContrib;
+  Foswiki::Contrib::BehaviourContrib::addHEAD();
   ....
 </verbatim>
 
@@ -24,12 +24,12 @@ sub commonTagsHandler {
 sub addHEAD {
     my $base = '%PUBURLPATH%/%SYSTEMWEB%/BehaviourContrib';
     my $USE_SRC =
-      TWiki::Func::getPreferencesValue('BEHAVIOURCONTRIB_DEBUG') ?
+      Foswiki::Func::getPreferencesValue('BEHAVIOURCONTRIB_DEBUG') ?
           '_src' : '';
     my $head = <<HERE;
 <script type='text/javascript' src='$base/behaviour$USE_SRC.js'></script>
 HERE
-    TWiki::Func::addToHEAD( 'BEHAVIOURCONTRIB', $head );
+    Foswiki::Func::addToHEAD( 'BEHAVIOURCONTRIB', $head );
 }
 
 1;
